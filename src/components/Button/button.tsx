d20d4e00ceaaa3dd6c3caab32ab7884e1510522e
lenny.cao <lenny.cao@h8tch.com>
2023-01-26 00:51:55 +0800

    feed:项目初始化以及button组件样式

---
+++ src/components/Button/button.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import classnames from 'classnames';
+
+export enum ButtonSize {
+	Large = 'lg',
+	Small = 'sm',
+}
+
+export enum ButtonType {
+	Primary = 'primary',
+	Default = 'default',
+	Danger = 'danger',
+	Link = 'link',
+}
+
+interface BaseButtonProps {
+	className?: string;
+	disabled?: boolean;
+	size?: ButtonSize;
+	btnType?: ButtonType;
+	children?: React.ReactNode;
+	href?: string;
+}
+
+const Button: React.FC<BaseButtonProps> = (props) => {
+	const { disabled, size, btnType, children, href } = props;
+	// btn, btn-lg, btn-primary
+	const classes = classnames('btn', {
+		[`btn-${btnType}`]: btnType,
+		[`btn-${size}`]: size,
+		disabled: btnType === ButtonType.Link && disabled,
+	});
+	if (btnType === ButtonType.Link && href) {
+		return (
+			<a href={href} className={classes}>
+				{children}
+			</a>
+		);
+	} else {
+		return (
+			<button className={classes} disabled={disabled}>
+				{children}
+			</button>
+		);
+	}
+};
+
+Button.defaultProps = {
+	disabled: false,
+	btnType: ButtonType.Default,
+};
+
+export default Button;
