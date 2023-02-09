a9ede6e52499a9109d1fcfad6ae6dd2ce645674d
lenny.cao <lenny.cao@h8tch.com>
2023-02-09 23:25:23 +0800

    feed:添加欢迎页面，更新readme

---
+++ src/webcome.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default {
+	title: 'Welcome/Welcome',
+};
+
+export const Types = () => (
+	<>
+		<h2>Welcome to use fobiz-ui react component library</h2>
+		<br />
+		<h4>Introduction:</h4>
+		<ul>
+			<li>🔥Typescript with React Hooks</li>
+			<li>🤳Use react-testing-library to make unit test</li>
+			<li>🎁Use storybook for native debugging and generating docs</li>
+			<li>✨Integrate React-Awesome for Icon component</li>
+		</ul>
+	</>
+);
